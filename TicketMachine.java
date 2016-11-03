2c6f7349e521dcd6466d150911a041d62d221461
Brian <nausikachampion69@gmail.com>
2016-11-03 10:52:17 +0100

    0456.a clase modificada

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,15 +17,18 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    // If the machine is special or not.
+    private boolean chachi;
 
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine(int cost,boolean especial)
     {
         price = cost;
         balance = 0;
         total = 0;
+        chachi = especial;
     }
 
     /**
@@ -68,6 +71,14 @@ public class TicketMachine
     public void printTicket()
     {
         if(balance >= price) {
+            if(chachi == true){
+                //Simulate the printing of the gift.
+                System.out.println("##################");
+                System.out.println("Congratulations,you are a winner");
+                System.out.println("Gift Ticket");
+                System.out.println("# The BlueJ Line#");
+                System.out.println("##################");
+            }
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
@@ -115,5 +126,5 @@ public class TicketMachine
         
         
         }
-}
+    }
 }
